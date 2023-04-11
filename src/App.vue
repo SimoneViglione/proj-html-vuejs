f7eaa9e63bb1fcc0cc8b54dd37cdaa40e1d7e4ff
Simone Viglione <zimoviglio51@gmail.com>
2023-04-11 16:22:17 +0200

    base header

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,27 @@
-<script setup>
+<script>
+    import PageHeader from './components/PageHeader.vue'
 
+    export default {
+        name: 'App',
+        components: {PageHeader},
+
+        data() {
+            return {
+                headerMenu: [
+                    {
+                        "logo": "https://static.dc.com/2023-03/LEGIONOB_Cv1_00111_DIGITAL.jpg?w=640",
+                        "headerNav": ["HOME", "MISSION", "CAUSES", "JOURNAL", "DONATE"]
+                    }
+                ]
+            }
+        }
+    }
 </script>
 
 <template>
-
+    <header>
+        <PageHeader :nav="headerMenu"/>
+    </header>
 </template>
 
 <style lang="scss">
---
+++ src/components/PageHeader.vue
@@ -0,0 +1,49 @@
+<script>
+    export default {
+        name: 'PageHeader',
+        props: ['nav'],
+
+        data() {
+            return {
+            }
+        }
+    }
+</script>
+
+<template>
+    <div class="jumbotron">
+        <div class="header-nav">
+            <img src="../img/avada-charity-logo.png" alt="avada logo" class="logo-header">
+
+            <ul>
+                <li v-for="(item, index) in nav.headerNav" :key="index">{{ item }}</li>
+            </ul>
+        </div>
+
+        <div class="slogan">
+            <h1>MAKE A DIFFERENCE</h1>
+            <p>As long as poverty, injustice & inequality persist, none of us can truly rest</p>
+            <button class="transparent-button"> OUR MISSION </button>
+            <button class="transparent-button"> DONATE NOW </button>
+        </div>
+    </div>
+</template>
+
+<style>
+    .jumbotron {
+        background-image: url(../img/home-page-slider.jpg);
+        background-size: cover;
+        height: 700px;
+        color: white;
+    }
+
+    .logo-header {
+        margin: 2rem 5rem;
+        height: 50px;
+    }
+
+    .slogan {
+        text-align: center;
+        margin-top: 12rem;
+    }
+</style>
